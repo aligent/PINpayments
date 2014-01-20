0facd5de9948723eb52d2079241dc6ce270f6821
Andrew Dwyer <andrew@aligent.com.au>
2014-01-21 10:07:40 +1030

    re #123 Added PinPayment logo to payment method

--- app/code/local/Dwyera/Pinpay/Block/Form.php
+++ app/code/local/Dwyera/Pinpay/Block/Form.php
@@ -5,8 +5,12 @@ class Dwyera_Pinpay_Block_Form extends Mage_Payment_Block_Form_Cc
     protected function _construct()
     {
         parent::_construct();
-        Mage::log('payment form block', Zend_Log::ERR, "dwyera_pinpay_controller.log", true);
-        $this->setTemplate('pinpay/form/pinpay.phtml');
+
+        $mark = Mage::getConfig()->getBlockClassName('core/template');
+        $mark = new $mark;
+        $mark->setTemplate('pinpay/form/mark.phtml');
+        $test = $mark->toHtml();
+        $this->setTemplate('pinpay/form/pinpay.phtml')->setMethodLabelAfterHtml($mark->toHtml());
     }
 
     /**
