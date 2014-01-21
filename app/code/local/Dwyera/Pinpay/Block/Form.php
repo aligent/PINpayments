21ec4357f221cba71d8412a937ed1a4ca3003659
Andrew Dwyer <andrew@aligent.com.au>
2014-01-21 14:27:54 +1030

    Add additional comments to payment form block and tidy code

--- app/code/local/Dwyera/Pinpay/Block/Form.php
+++ app/code/local/Dwyera/Pinpay/Block/Form.php
@@ -5,11 +5,10 @@ class Dwyera_Pinpay_Block_Form extends Mage_Payment_Block_Form_Cc
     protected function _construct()
     {
         parent::_construct();
-
         $mark = Mage::getConfig()->getBlockClassName('core/template');
         $mark = new $mark;
         $mark->setTemplate('pinpay/form/mark.phtml');
-        $test = $mark->toHtml();
+        // Appends the "Powered by PinPayments logo to payment method description
         $this->setTemplate('pinpay/form/pinpay.phtml')->setMethodLabelAfterHtml($mark->toHtml());
     }
 
