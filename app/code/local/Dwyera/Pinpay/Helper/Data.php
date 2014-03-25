2cbfc229eff7e28a2afe098690204ecd689e23aa
Ben Turner <ben@aligent.com.au>
2014-03-25 17:29:02 +1030

    Added helper to set test or production script

--- app/code/local/Dwyera/Pinpay/Helper/Data.php
+++ app/code/local/Dwyera/Pinpay/Helper/Data.php
@@ -1,5 +1,12 @@
 <?php
 class Dwyera_Pinpay_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    public function getScriptUrl(){
+        if(Mage::getStoreConfig('payment/pinpay/test')){
+            return 'pinpay/pin.test.js';
+        }else{
+            return 'pinpay/pin.prod.js';
+       }
+    }
 }
 	 
