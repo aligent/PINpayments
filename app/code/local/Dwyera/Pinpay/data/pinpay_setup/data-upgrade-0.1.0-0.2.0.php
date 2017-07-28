0724961f0bc4247adfd812d31cbb305644bc1ccf
Ramya Rangashamaiah <ramya@aligent.com.au>
2017-07-28 13:15:38 +0930

    Adding setup script to hold customer token and creditcard display number(last 4 digits) in database

---
+++ app/code/local/Dwyera/Pinpay/data/pinpay_setup/data-upgrade-0.1.0-0.2.0.php
@@ -0,0 +1,34 @@
+<?php
+/** @var Mage_Core_Model_Resource_Setup $installer */
+
+$installer = new Mage_Customer_Model_Entity_Setup('core_setup');
+
+$installer->startSetup();
+
+
+/* @var $eavConfig Mage_Eav_Model_Config */
+$installer->addAttribute('customer', 'pinpayment_customer_token', array(
+    'type'      => 'varchar',
+    'label'     => 'Pinpayment customer token',
+    'input'     => 'text',
+    'visible'   => 0,
+    'required'  => 0,
+    'position'  => 1,
+    'required'  => false,
+    'default'   => 1,
+    'user_defined' => true
+));
+
+/* @var $eavConfig Mage_Eav_Model_Config */
+$installer->addAttribute('customer', 'pinpayment_card_display_number', array(
+    'type'      => 'varchar',
+    'label'     => 'Pinpayment customer card display number',
+    'input'     => 'text',
+    'visible'   => 0,
+    'required'  => 0,
+    'position'  => 1,
+    'required'  => false,
+    'default'   => 1,
+    'user_defined' => true
+));
+$installer->endSetup();
